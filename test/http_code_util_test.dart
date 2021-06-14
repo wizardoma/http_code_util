ead06bdb4eb79f7b5420bcd7b294b845fa6aa8df
Ibekason Alexander <alibekason@gmail.com>
2021-06-14 17:11:14 +0100

    add tests

--- test/http_code_util_test.dart
+++ test/http_code_util_test.dart
@@ -3,10 +3,22 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:http_code_util/http_code_util.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('verifies status code checks', () {
+    final util = HttpCodeUtil();
+    expect(util.isSuccessful(200), true);
+    expect(util.isSuccessful(300), false);
+
+    expect(util.isClientError(456), true);
+    expect(util.isClientError(399), false);
+
+    expect(util.isInformation(102), true);
+    expect(util.isInformation(99), false);
+
+    expect(util.isServerError(512), true);
+    expect(util.isServerError(443), false);
+
+    expect(util.isRedirect(302), true);
+    expect(util.isRedirect(202), false);
+
   });
 }
