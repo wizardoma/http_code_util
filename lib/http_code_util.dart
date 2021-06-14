cac5b27392c8744326ca15f0a79fb63aa65e3d77
Ibekason Alexander <alibekason@gmail.com>
2021-06-14 21:16:33 +0100

    rename class

--- lib/http_code_util.dart
+++ lib/http_code_util.dart
@@ -5,291 +5,291 @@ import 'package:http_code_util/status_codes.dart';
 /// @author: Ibekason Alexander Onyebuchi
 
 
-class HttpCodeUtil {
+class StatusCodeUtil {
   /// Returns true if code ranges between 200 and 300
-  bool isSuccessful(int code) {
+  static bool isSuccessful(int code) {
     return code >= 200 && code <= 299;
   }
 
   /// Returns true if code ranges between 100 and 200
-  bool isInformation(int code) {
+  static bool  isInformation(int code) {
     return code >= 100 && code <= 199;
   }
 
   /// Returns true if code ranges between 300 and 400
-  bool isRedirect(int code) {
+  static bool isRedirect(int code) {
     return code >= 300 && code <= 399;
   }
 
   /// Returns true if code ranges between 400 and 499
-  bool isClientError(int code) {
+  static bool isClientError(int code) {
     return code >= 400 && code <= 499;
   }
 
   /// Returns true if code ranges between 500 and 599
-  bool isServerError(int code) {
+  static bool isServerError(int code) {
     return code >= 500 && code <= 599;
   }
 
   // information responses
 
-  bool isContinue(int code) {
+  static bool isContinue(int code) {
     return code == CONTINUE;
   }
 
-  bool isSwitchProtocol(int code) {
+  static bool isSwitchProtocol(int code) {
     return code == SWITCHING_PROTOCOL;
   }
 
-  bool isProcessing(int code) {
+  static bool isProcessing(int code) {
     return code == PROCESSING;
   }
 
-  bool isEarlyHints(int code) {
+  static bool isEarlyHints(int code) {
     return code == EARLY_HINTS;
   }
 
   // successful responses
 
-  bool isOk(int code) {
+  static bool isOk(int code) {
     return code == OK;
   }
 
-  bool isCreated(int code) {
+  static bool isCreated(int code) {
     return code == CREATED;
   }
 
-  bool isAccepted(int code) {
+  static bool isAccepted(int code) {
     return code == ACCEPTED;
   }
 
-  bool isNonAuthInfo(int code) {
+  static bool isNonAuthInfo(int code) {
     return code == NONAUTHORITATIVE_INFORMATION;
   }
 
-  bool isNoContent(int code) {
+  static bool isNoContent(int code) {
     return code == NO_CONTENT;
   }
 
-  bool isResetContent(int code) {
+  static bool isResetContent(int code) {
     return code == RESET_CONTENT;
   }
 
-  bool isPartialContent(int code) {
+  static bool isPartialContent(int code) {
     return code == PARTIAL_CONTENT;
   }
 
-  bool isMultiStatus(int code) {
+  static bool isMultiStatus(int code) {
     return code == MULTI_STATUS;
   }
 
-  bool isAlreadyReported(int code) {
+  static bool isAlreadyReported(int code) {
     return code == ALREADY_REPORTED;
   }
 
-  bool isImUsed(int code) {
+  static bool isImUsed(int code) {
     return code == IM_USED;
   }
 
   // Redirection Responses
 
-  bool isMultiChoice(int code) {
+  static bool isMultiChoice(int code) {
     return code == MULTIPLE_CHOICE;
   }
 
-  bool isMoved(int code) {
+  static bool isMoved(int code) {
     return code == MOVED_PERMANENTLY;
   }
 
-  bool isFound(int code) {
+  static bool isFound(int code) {
     return code == FOUND;
   }
 
-  bool isSeeOther(int code) {
+  static bool isSeeOther(int code) {
     return code == SEE_OTHER;
   }
 
-  bool isNotModified(int code) {
+  static bool isNotModified(int code) {
     return code == NOT_MODIFIED;
   }
 
-  bool isUseProxy(int code) {
+  static bool isUseProxy(int code) {
     return code == USE_PROXY;
   }
 
-  bool isUnused(int code) {
+  static bool isUnused(int code) {
     return code == UNUSED;
   }
 
-  bool isTempRedirect(int code) {
+  static bool isTempRedirect(int code) {
     return code == TEMPORARY_REDIRECT;
   }
 
-  bool isPermRedirect(int code) {
+  static bool isPermRedirect(int code) {
     return code == PERMANENT_REDIRECT;
   }
 
   // Client error responses
 
-  bool isBadRequest(int code) {
+  static bool isBadRequest(int code) {
     return code == BAD_REQUEST;
   }
 
-  bool isForbidden(int code) {
+  static bool isForbidden(int code) {
     return code == FORBIDDEN;
   }
 
-  bool isUnauthorized(int code) {
+  static bool isUnauthorized(int code) {
     return code == UNAUTHORIZED;
   }
 
-  bool isPaymentRequired(int code) {
+  static bool isPaymentRequired(int code) {
     return code == PAYMENT_REQUIRED;
   }
 
-  bool isNotFound(int code) {
+  static bool isNotFound(int code) {
     return code == NOT_FOUND;
   }
 
-  bool isMethodNotAllowed(int code) {
+  static bool isMethodNotAllowed(int code) {
     return code == METHOD_NOTALLOWED;
   }
 
-  bool isNotAcceptable(int code) {
+  static bool isNotAcceptable(int code) {
     return code == NOT_ACCEPTABLE;
   }
 
-  bool isProxyAuthRequired(int code) {
+  static bool isProxyAuthRequired(int code) {
     return code == PROXY_AUTH_REQUIRED;
   }
 
-  bool isRequestTimeout(int code) {
+  static bool isRequestTimeout(int code) {
     return code == REQUEST_TIMEOUT;
   }
 
-  bool isConflict(int code) {
+  static bool isConflict(int code) {
     return code == CONFLICT;
   }
 
-  bool isGone(int code) {
+  static bool isGone(int code) {
     return code == GONE;
   }
 
-  bool isLengthRequired(int code) {
+  static bool isLengthRequired(int code) {
     return code == LENGTH_REQUIRED;
   }
 
-  bool isPreconditionedFailed(int code) {
+  static bool isPreconditionedFailed(int code) {
     return code == PRECONDITION_FAILED;
   }
 
-  bool isPayloadTooLarge(int code) {
+  static bool isPayloadTooLarge(int code) {
     return code == PAYLOAD_TOO_LARGE;
   }
 
-  bool isUriTooLong(int code) {
+  static bool isUriTooLong(int code) {
     return code == URI_TOO_LONG;
   }
 
-  bool isUnsupportedMedia(int code) {
+  static bool isUnsupportedMedia(int code) {
     return code == UNSUPPORTED_MEDIA_TYPE;
   }
 
-  bool isRangeNotSatisfiable(int code) {
+  static bool isRangeNotSatisfiable(int code) {
     return code == RANGE_NOT_SATISFIABLE;
   }
 
-  bool isExpectationFailed(int code) {
+  static bool isExpectationFailed(int code) {
     return code == EXPECTATION_FAILED;
   }
 
-  bool isImATeapot(int code) {
+  static bool isImATeapot(int code) {
     return code == IMATEAPOT;
   }
 
-  bool isMisdirectedRequest(int code) {
+  static bool isMisdirectedRequest(int code) {
     return code == MISDIRECTED_REQUEST;
   }
 
-  bool isUnprocessedEntity(int code) {
+  static bool isUnprocessedEntity(int code) {
     return code == UNPROCESSABLE_ENTITY;
   }
 
-  bool isLocked(int code) {
+  static bool isLocked(int code) {
     return code == LOCKED;
   }
 
-  bool isFailedDependency(int code) {
+  static bool isFailedDependency(int code) {
     return code == FAILED_DEPENDENCY;
   }
 
-  bool isTooEarly(int code) {
+  static bool isTooEarly(int code) {
     return code == TOO_EARLY;
   }
 
-  bool isUpgradeRequired(int code) {
+  static bool isUpgradeRequired(int code) {
     return code == UPGRADE_REQUIRED;
   }
 
-  bool isPreconditionRequired(int code) {
+  static bool isPreconditionRequired(int code) {
     return code == PRECONDITION_REQUIRED;
   }
 
-  bool isTooManyRequests(int code) {
+  static bool isTooManyRequests(int code) {
     return code == TOO_MANY_REQUESTS;
   }
 
-  bool isRequestHeaderLong(int code) {
+  static bool isRequestHeaderLong(int code) {
     return code == REQUEST_HEADER_FILES_TOO_LONG;
   }
 
-  bool isLegalReasons(int code) {
+  static bool isLegalReasons(int code) {
     return code == LEGAL_REASONS;
   }
 
   // server responses
 
-  bool isInternalServerError(int code) {
+  static bool isInternalServerError(int code) {
     return code == INTERNAL_SERVER_ERROR;
   }
 
-  bool isNotImplemented(int code) {
+  static bool isNotImplemented(int code) {
     return code == NOT_IMPLEMENTED;
   }
 
-  bool isBadGateway(int code) {
+  static bool isBadGateway(int code) {
     return code == BAD_GATEWAY;
   }
 
-  bool isServiceUnavailable(int code) {
+  static bool isServiceUnavailable(int code) {
     return code == SERVICE_UNAVAILABLE;
   }
 
-  bool isGatewayTimeout(int code) {
+  static bool isGatewayTimeout(int code) {
     return code == GATEWAY_TIMEOUT;
   }
 
-  bool isHttpNotSupported(int code) {
+  static bool isHttpNotSupported(int code) {
     return code == HTTP_NOT_SUPPORTED;
   }
 
-  bool isVariantAlso(int code) {
+  static bool isVariantAlso(int code) {
     return code == VARIANT_ALSO_NEGOTIATES;
   }
 
-  bool isSufficientStorage(int code) {
+  static bool isSufficientStorage(int code) {
     return code == INSUFFICIENT_STORAGE;
   }
 
-  bool isLoopDetected(int code) {
+  static bool isLoopDetected(int code) {
     return code == LOOP_DETECTED;
   }
 
-  bool isNotExtended(int code) {
+  static bool isNotExtended(int code) {
     return code == NOT_EXTENDED;
   }
 
-  bool isNetworkAuthError(int code) {
+  static bool isNetworkAuthError(int code) {
     return code == NETWORK_AUTHENTICATION_ERROR;
   }
 }
--- test/http_code_util_test.dart
+++ test/http_code_util_test.dart
@@ -4,21 +4,20 @@ import 'package:http_code_util/http_code_util.dart';
 
 void main() {
   test('verifies status code checks', () {
-    final util = HttpCodeUtil();
-    expect(util.isSuccessful(200), true);
-    expect(util.isSuccessful(300), false);
+    expect(StatusCodeUtil.isSuccessful(200), true);
+    expect(StatusCodeUtil.isSuccessful(300), false);
 
-    expect(util.isClientError(456), true);
-    expect(util.isClientError(399), false);
+    expect(StatusCodeUtil.isClientError(456), true);
+    expect(StatusCodeUtil.isClientError(399), false);
 
-    expect(util.isInformation(102), true);
-    expect(util.isInformation(99), false);
+    expect(StatusCodeUtil.isInformation(102), true);
+    expect(StatusCodeUtil.isInformation(99), false);
 
-    expect(util.isServerError(512), true);
-    expect(util.isServerError(443), false);
+    expect(StatusCodeUtil.isServerError(512), true);
+    expect(StatusCodeUtil.isServerError(443), false);
 
-    expect(util.isRedirect(302), true);
-    expect(util.isRedirect(202), false);
+    expect(StatusCodeUtil.isRedirect(302), true);
+    expect(StatusCodeUtil.isRedirect(202), false);
 
   });
 }
